e26f0cd6f8510e36d09e6ec24c955278f266f42f
sjadriana <adriana.sj@live.com>
2020-07-10 20:59:38 -0300

    cli correção

--- cli.js
+++ cli.js
@@ -1,28 +1,26 @@
 #!/usr/bin/env node
-const argv=require('./commands').argv
 const colors = require('colors')
-const { mdLinks,validateFile } = require('./index.js')
+const { mdLinks, validateFile } = require('./index.js')
 //Se você usa as informações da posição 0 do process.arg()
-const command=argv._[0]
+const command = process.argv[2]
 console.log(command)
-
 
 //Switch avalie cada case na posição 0 do código process.argv
 switch(command){
   case '--validate':
-    console.log(colors.yellow.bold('VALIDATED LINKS'))
+      console.log(colors.yellow.bold('VALIDATED LINKS'))
     //validateLinks(argv.file)
     mdLinks(argv.file,'--validate')
     break;
   case '--stats':
     console.log(colors.yellow.bold('STATS'))
-    mdLinks(argv.file,"--stats")
+    mdLinks(argv.file,'--stats')
     break;
   case '--validateAndStats':
     console.log(colors.yellow.bold('VALIDATED LINKS & STATS'))
-    validateFile(argv.file,"--validateAndStats")
+    validateFile(argv.file,'--validateAndStats')
     break;
   default:
-    console.log(colors.yellow.bold('NEED HELP? --help'))
-}
+    console.log(colors.yellow.bold(''))
+  }
   
--- index.js
+++ index.js
@@ -6,90 +6,110 @@ const colors = require('colors')
 
 // (valida se é arquivo .md)
 function validateFile(file,options){
-    if(path.extname(file) === '.md'){
-      // console.log(       file)
-        mdLinks(file,options)
+  if(path.extname(file) === '.md'){
+   // console.log(       file)
+    mdLinks(file,options)
     }else{
-        console.log(colors.magenta.bold('Plese insert a valid file(.md)'))
+      console.log(colors.magenta.bold('Plese insert a valid file(.md)'))
     }
 } 
 validateFile(process.argv[2], process.argv[3])
 
 //lendo arquivo 
 function mdLinks(file,options){
-    const string = fs.readFileSync(file, 'utf-8')
+  const string = fs.readFileSync(file, 'utf-8')
     //console.log(string)
-    getLinks(string,options)
+  getLinks(string,options)
 }
-
-// function mdLinks1 (file, options){
-//     const regexMdlinks = /([^[]+)\](\([^)]*)/gm
-//     const arr = [];
-//   return new Promise ((resolve, rejects) => {
-//     const string = fs.readFile(file,'utf8', (err, data) => {
-//     if (err){
-//         rejects (err.message);
-//     } else {
-//         const regex = data.match(regexMdlinks)
-//         regex.forEach((el) => {
-//         const Text = el.split('](');
-//         const title = Text[0].replace('\n','');
-//         const href =Text[1]
-//         arr.push( {title, href, file})
-//         })
-//         resolve(arr)
-//         console.log(arr)
-//     }
-//     console.log(string)
-//     getLinks(string,options)
-//     });
-//     });
-// }
-
-
-
-
 //pegando links
 function getLinks(string,options){
-    const expReg = /(https?:)([\w\.\/\-\#\?\=\&]+)/g
-    const links = string.match(expReg)
-     //console.log(links)
-        if(options==="validateAndStats"){
-            validateAndStats(links)
-        }else if(options==="--validate"){
-            onlyValidate(links ) //chamar arr aqui
-        }else if(options==="--stats"){
-            onlyStats(links)
-        } else  {
-          //função retornando links sem validação
-        
-        }
+  const expReg = /(https?:)([\w\.\/\-\#\?\=\&]+)/g
+  const links = string.match(expReg)
+   //console.log(links)
+    if(options==="validateAndStats"){
+      validateAndStats(links)
+    } else if(options==="--validate"){
+      onlyValidate(links ) //chamar arr aqui
+    }else if(options==="--stats"){
+      onlyStats(links)
+    } else {
+      //função retornando links sem validação    
+    }
 }
 function readFile (file){
-    const regexMdlinks = /([^[]+)\](\([^)]*)/gm
-    const arr = [];
-  return new Promise ((resolve, rejects) => {
-    fs.readFile(file,'utf8', (err, data) => {
-    if (err){
-        rejects (err.message);
-    } else {
-        const regex = data.match(regexMdlinks)
-        regex.forEach((el) => {
-        const Text = el.split('](');
-        const title = Text[0].replace('\n','');
-        const href =Text[1]
-        arr.push( {title, href, file})
-        })
-        resolve(arr)
-        console.log(arr)
-    }
-    });
-    });
+  const regexMdlinks = /([^[]+)\](\([^)]*)/gm
+  const arr = [];
+return new Promise ((resolve, rejects) => {
+  fs.readFile(file,'utf8', (err, data) => {
+  if (err){
+    rejects (err.message);
+  } else { // add else para validate
+    const regex = data.match(regexMdlinks)
+    regex.forEach((el) => {
+    const Text = el.split('](');
+    const title = Text[0].replace('\n','');
+    const href =Text[1]
+    arr.push( {title, href, file})
+    })
+    resolve(arr)
+    console.log(arr)
+  }
+  });
+  });
 }
-  readFile(process.argv[2])
+readFile(process.argv[2])
+
+//pegando os links
+function onlyValidate(links){ 
+    //console.log(links)
+  let promises = links.map(link => fetch(link)
+  .then (res => {
+  let resp = {
+    url: res.url,
+    status: res.status,
+    text: res.statusText
+    }
+    if(resp.status === 200){
+      console.log(colors.magenta(res.status), colors.cyan(res.statusText), colors.blue(('url:', link)))
+    }else{
+      console.log(colors.yellow(res.status), colors.green(res.statusText),('url:', link))
+    }
+    return resp
+    })
+
+    .catch (error => {
+        let err = {
+          url: link,
+          status: 'error',
+          text: error.errno
+        }
+        if(err.status === 'error'){
+          console.log(colors.yellow(err.status), colors.green(err.text), ('url:', link))
+        }
+        return err
+      })
+      )
+      return Promise.all(promises)
+}
+function  onlyStats (res){
+  console.log(colors.rainbow('Total links: '), (res.length))
+  console.log(colors.magenta.bold('Broken links: ', res.reduce((accountant, element) => {
+    if (element.status !== 200){
+      return accountant += 1
+    }
+    return accountant
+      },0)))
+    console.log(colors.green.bold('Functional links: ', res.reduce((accountant, elemento) => {
+      if (elemento.status === 200){
+        return accountant += 1
+      }
+      return accountant
+      },0)))
+    return res
+}
 
 
-//Validando os links 
+ //Validando os links 
 function validateAndStats(links){ 
     let promises = links.map(link => fetch(link)
         .then (res => {
@@ -105,7 +125,7 @@ function validateAndStats(links){
             console.log(colors.yellow(res.status), colors.green(res.statusText),('url:', link))
         }
         return resp
-        
+    
     })
     .catch (error => {
         let err = {
@@ -123,58 +143,13 @@ function validateAndStats(links){
     .then(res =>  onlyStats(res))
 }
 
-function  onlyStats (res){
-    console.log(colors.rainbow('Total links: '), (res.length))
-    console.log(colors.magenta.bold('Broken links: ', res.reduce((accountant, element) => {
-        if (element.status !== 200){
-            return accountant += 1
-        }
-        return accountant
-        },0)))
-    console.log(colors.green.bold('Functional links: ', res.reduce((accountant, elemento) => {
-        if (elemento.status === 200){
-            return accountant += 1
-        }
-        return accountant
-        },0)))
 
-    return res
-}
 
-//pegando os links
-function onlyValidate(links){ //console.log(links)
-    let promises = links.map(link => fetch(link)
-        .then (res => {
-        let resp = {
-            url: res.url,
-            status: res.status,
-            text: res.statusText
-        }
-            if(resp.status === 200){
-                console.log(colors.magenta(res.status), colors.cyan(res.statusText), colors.blue(('url:', link)))
-            }else{
-                console.log(colors.yellow(res.status), colors.green(res.statusText),('url:', link))
-            }
-            return resp
-    })  
-    .catch (error => {
-        let err = {
-            url: link,
-            status: 'error',
-            text: error.errno
-        }
-            if(err.status === 'error'){
-                console.log(colors.yellow(err.status), colors.green(err.text), ('url:', link))
-            }
-            return err
-    })
-    )
-    return Promise.all(promises)
-}
 
 
 module.exports = {
-    mdLinks,
-    validateFile
+    mdLinks, validateFile
+    // readFile,
+    // validateFile
 }
 
