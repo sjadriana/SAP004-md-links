438746b5b7efaab69984e7ad7d76d26c766febba
sjadriana <adriana.sj@live.com>
2020-08-25 20:59:32 -0300

    refatoração

--- src/md-links.js
+++ src/md-links.js
@@ -1,12 +1,12 @@
-// const fs = require('fs');
-// const readFile = require('./read-file');
-// const validateFileExtension = require('./validate-file-extension');
-// const { command } = require('yargs')
+const fs = require('fs');
+const readFile = require('./read-file');
+const validateFileExtension = require('./validate-file-extension');
+const { command } = require('yargs')
 
-// module.exports = (file, options) => {
-//   validateFileExtension(file);
-//   readFile(file, options).then((response) => {    
-//     console.log("here response md-links");
-//     console.log(response);
-//   });
-// };
+module.exports = (file, options) => {
+  validateFileExtension(file);
+  readFile(file, options).then((response) => {
+    console.log("here response md-links");
+    console.log(response);
+  });
+};
--- src/validate-link.js
+++ src/validate-link.js
@@ -1,19 +1,19 @@
-// const fetch = require('node-fetch')
+const fetch = require('node-fetch')
 
-// module.exports = (data) => {
-//   return new Promise((resolve) => {
-//     fetch(data.href)
-//       .then(response => {
-//         const { status } = response;
-//         data.status = status;
-//         data.statusMessage = "ok";
-//         return resolve(data);
-//       })
-//       .catch(err => {
-//         const { status } = err;
-//         data.status = status ? status : 404;
-//         data.statusMessage = "fail";
-//         return resolve(data);
-//       })
-//   });
-// }
+module.exports = (data) => {
+  return new Promise((resolve) => {
+    fetch(data.href)
+      .then(response => {
+        const { status } = response;
+        data.status = status;
+        data.statusMessage = "ok";
+        return resolve(data);
+      })
+      .catch(err => {
+        const { status } = err;
+        data.status = status ? status : 404;
+        data.statusMessage = "fail";
+        return resolve(data);
+      })
+  });
+}
